0d2a58e106aa9e838bd224e6a284886146291020
Topher Brown <topher200@gmail.com>
2015-06-20 02:33:38 -0400

    make my scroll bind map to only normal evil-mode

--- topher/keybindings.el
+++ topher/keybindings.el
@@ -1,10 +1,11 @@
+; global keybindings
 (define-key global-map (kbd "C-<tab>") 'evil-window-next)
 (define-key global-map (kbd "C-S-<tab>") 'evil-window-prev)
 (define-key global-map (kbd "C-s") 'save-buffer)
-(define-key global-map (kbd "C-j") 'evil-scroll-down)
-(define-key global-map (kbd "C-k") 'evil-scroll-up)
 (define-key global-map (kbd "C-c C-s") 'topher-named-shell)
 
 ; evil mode defines
+(define-key evil-normal-state-map (kbd "C-j") 'evil-scroll-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-scroll-up)
 (define-key evil-normal-state-map (kbd "Y") "y$")
 (define-key evil-normal-state-map (kbd "M-;") 'topher-comment-dwim-line)
