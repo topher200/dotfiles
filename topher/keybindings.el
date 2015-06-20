4b59da93382882a14c62e6e3f44e6644b6ff7415
Topher Brown <chris.brown@farsounder.com>
2015-06-19 22:50:38 -0400

    add the topher package

---
+++ topher/keybindings.el
@@ -0,0 +1,3 @@
+(define-key global-map (kbd "C-<tab>") 'next-multiframe-window)
+(define-key global-map (kbd "C-S-<tab>") 'previous-multiframe-window)
+(define-key global-map (kbd "C-s") 'save-buffer)
