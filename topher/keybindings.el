c05f03fb1da6470c0e0b62fb91611b637e29bfb1
Topher Brown <chris.brown@farsounder.com>
2015-06-20 22:32:25 -0400

    adapt my keybindings to not use meta keys
    The C-c <lower or uppercase letter> region is saved for the user, but not
    so much if you add meta keys

--- topher/keybindings.el
+++ topher/keybindings.el
@@ -2,8 +2,8 @@
 (define-key global-map (kbd "C-<tab>") 'evil-window-next)
 (define-key global-map (kbd "C-S-<tab>") 'evil-window-prev)
 (define-key global-map (kbd "C-s") 'save-buffer)
-(define-key global-map (kbd "C-c C-s") 'topher-named-shell)
-(define-key global-map (kbd "C-c C-k") 'kill-this-buffer)
+(define-key global-map (kbd "C-c s") 'topher-named-shell)
+(define-key global-map (kbd "C-c k") 'kill-this-buffer)
 
 ; evil mode defines
 (define-key evil-normal-state-map (kbd "C-j") 'evil-scroll-down)
