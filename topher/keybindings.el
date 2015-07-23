2f6ebb4587d296d9103ba8e7fc706530000561fa
Topher Brown <chris.brown@farsounder.com>
2015-07-23 15:23:09 -0400

    keybindings: revert-buffer without confirmation

--- topher/keybindings.el
+++ topher/keybindings.el
@@ -5,7 +5,10 @@
 (define-key global-map (kbd "C-c s") 'topher-named-shell)
 (define-key global-map (kbd "C-c k") 'kill-this-buffer)
 (define-key global-map (kbd "C-c e") 'erase-buffer)
-(define-key global-map (kbd "C-c r") 'revert-buffer)
+
+; revert buffer without confirmation
+(define-key global-map (kbd "C-c r")
+  (lambda () (interactive) (revert-buffer t t)))
 
 ; evil mode defines
 (define-key evil-normal-state-map (kbd "C-j") 'evil-scroll-down)
