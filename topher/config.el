2f278df784bd963943d3167b6d4de810b86fc2c0
Topher Brown <t.brown@wordstream.com>
2016-02-06 21:42:44 -0500

    remove `grin` config

--- topher/config.el
+++ topher/config.el
@@ -28,34 +28,6 @@
     ad-do-it))
 (ad-activate 'hippie-expand)
 
-;; python grin support for emacs. Based on ack support script from:
-;; http://stackoverflow.com/questions/2322389/ack-does-not-work-when-run-from-grep-find-in-emacs-on-windows
-
-(defcustom grin-command (or (executable-find "grin")
-                            (executable-find "grin-grep"))
-  "Command to use to call grin"
-  :type 'file)
-
-(defvar grin-command-line (concat grin-command " --emacs -D "))
-(defvar grin-history nil)
-(defvar grin-host-defaults-alist nil)
-
-(defun grin ()
-  "Like ack, but using grin as the default"
-  ; Make sure grep has been initialized
-  (interactive)
-  (if (>= emacs-major-version 22)
-      (require 'grep)
-    (require 'compile))
-  ; Close STDIN to keep grin from going into filter mode
-  (let ((null-device (format "< %s" null-device))
-        (grep-command grin-command-line)
-        (grep-history grin-history)
-        (grep-host-defaults-alist grin-host-defaults-alist))
-    (call-interactively 'grep)
-    (setq grin-history             grep-history
-          grin-host-defaults-alist grep-host-defaults-alist)))
-
 (defun after-load-python ()
   (defun python-fill-string (&optional justify)
     (let* ((str-start-pos
