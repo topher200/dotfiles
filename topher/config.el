0599c81c6d7c21d409fc9f56dc2177f90aa42fa7
Topher Brown <topher200@gmail.com>
2017-02-08 22:13:14 -0500

    add typescript mode for underscore word jumping

--- topher/config.el
+++ topher/config.el
@@ -13,6 +13,7 @@
 (add-hook 'text-mode-hook 'underscores-in-words)
 (add-hook 'robot-mode-hook 'underscores-in-words)
 (add-hook 'dockerfile-mode-hook 'underscores-in-words)
+(add-hook 'typescript-mode-hook 'underscores-in-words)
 
 ;; save the desktop periodically
 (add-hook 'projectile-idle-timer-hook 'desktop-save-in-desktop-dir)
