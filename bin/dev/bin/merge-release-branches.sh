934ed3897bd53fe19af0ad9646ab4298371e0050
Topher Brown <t.brown@wordstream.com>
2018-04-09 17:37:52 -0400

    smarter release branch checker

--- bin/dev/bin/merge-release-branches.sh
+++ bin/dev/bin/merge-release-branches.sh
@@ -6,8 +6,12 @@ git pull # POSSIBLY DANGEROUS!
 printf '\n--- starting merges now. everything commit below this point should print "Already up-to-date." ---\n\n'
 
 # look for all remote branches. get just the release-20** ones. awk the output
-# so we just get the 'branch name' column
-branches=`git ls-remote origin | grep refs/heads | grep release-20.. | awk '{print $1}'`
+# so we just get the 'branch name' column. we use a crazy regex so we don't get
+# branch names like 'release-2018-01-01-migrators'.
+branches=`git ls-remote origin |
+          grep refs/heads |
+          grep 'refs/heads/release-[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$' |
+          sed -e 's/^.*refs/heads//'`
 for branch in $branches; do
     echo "trying to merge commit id $branch"
     git merge $branch
