3f2a0a68f532bf8cead710f082fcce731e71c6da
Topher Brown <t.brown@wordstream.com>
2019-09-29 22:56:33 -0400

    update wordstream scripts
    Former-commit-id: 5675a45a2efa2a448d8f86eb08f879dd9d37ffbd

--- bin/dev/bin/check_date_differences_postgres.py
+++ bin/dev/bin/check_date_differences_postgres.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import subprocess
+import asyncio
+import logging
 
 import retry # pip install retry
 
 
 MIN_ACCOUNT_ID = 0
-MAX_ACCOUNT_ID = 76000
+MAX_ACCOUNT_ID = 80000
 
 # certain account IDs are known to take too long to query against (AWS2)
 TAKES_TOO_LONG = {
@@ -13,41 +14,74 @@ TAKES_TOO_LONG = {
     75691,
 }
 
-RANGE_INCREMENT = 1
+RANGE_INCREMENT = 10
+NUM_PARALLEL_JOBS = 10
 POSTGRES_CALL = '" SELECT pr.account_id, Max(pr.date)  AS "performance_report_max_date", Max(prs.date) AS "performance_report_segmented_max_date" FROM performance_report AS pr JOIN performance_report_segmented AS prs ON pr.account_id = prs.account_id WHERE pr.date > \'2019-06-23\' and prs.date > \'2019-06-23\' and pr.account_id >= {} AND pr.account_id < {} GROUP BY pr.account_id HAVING Max(pr.date) != Max(prs.date); "'
 RUN_COMMAND = "export PGPASSWORD='***REMOVED***'; psql -U tbrown -h readonly.aws2.wordstream.com --no-psqlrc --tuples-only --no-align --field-separator ',' -d adwords -c {}"
 
 
-@retry.retry(subprocess.CalledProcessError, tries=10, delay=.1, backoff=2)
-def __make_psql_call(run_command:str) -> str:
-    return subprocess.check_output(
-        run_command,
-        shell=True,
-        text=True,
-        stderr=subprocess.STDOUT,
-    )
+class ShellCallError(Exception):
+    pass
 
-def __query_account_id_range(account_id_min, account_id_max):
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('asyncio').setLevel(logging.WARN)
+
+
+@retry.retry(ShellCallError, tries=10, delay=.1, backoff=2)
+async def __make_psql_call(run_command:str) -> str:
+    proc = await asyncio.create_subprocess_shell(run_command)
+    stdout, stderr = await proc.communicate()
+    await proc.wait()
+    if proc.returncode != 0 or stderr:
+        logging.debug('- subprocess failed. call: "%s", response: "%s"', run_command, stderr)
+        raise ShellCallError('call failed. request: "%s", response: "%s"' % (run_command, stderr))
+    return stdout
+
+
+async def __query_account_id_range(account_id_min:int, account_id_max:int) -> str:
     query = POSTGRES_CALL.format(account_id_min, account_id_max)
     run_command = RUN_COMMAND.format(query)
     try:
-        return __make_psql_call(run_command)
-    except subprocess.CalledProcessError as e:
-        print('postgres call failed. request:', e.cmd, 'response:', e.output)
-        return e.output
+        res = await __make_psql_call(run_command)
+    except ShellCallError as e:
+        logging.debug('postgres call failed permanently. %s', e)
+        return '%s' % e
+    else:
+        return res
+
+
+async def __consume_command_jobs(consumer_number:int, queue:asyncio.Queue) -> None:
+    while True:
+        start, end = await queue.get()
+        try:
+            logging.debug('- running from %s to %s on consumer #%s', start, end, consumer_number)
+            res = await __query_account_id_range(start, end)
+            if res and res.strip():
+                logging.info(res.strip())
+            logging.debug('- ran from %s to %s on consumer #%s', start, end, consumer_number)
+        except Exception:
+            logging.exception('error running consumed job')
+        finally:
+            queue.task_done()
+
+
+async def __run_jobs() -> None:
+    queue = asyncio.Queue()
+    consumers = [asyncio.create_task(__consume_command_jobs(i, queue)) for i in range(NUM_PARALLEL_JOBS)]
+    for start in range(MIN_ACCOUNT_ID, MAX_ACCOUNT_ID, RANGE_INCREMENT):
+        end = start + RANGE_INCREMENT
+        if any(start <= i < end for i in TAKES_TOO_LONG):
+            logging.debug('- skipping range: covers an account in %s', TAKES_TOO_LONG)
+            continue
+        await queue.put((start, end))
+    await queue.join()
+    for c in consumers:
+        c.cancel()
 
 
 def main():
-    for start in range(MIN_ACCOUNT_ID, MAX_ACCOUNT_ID, RANGE_INCREMENT):
-        end = start + RANGE_INCREMENT
-        print('- running from {} to {}'.format(start, end))
-        if any(start <= i < end for i in TAKES_TOO_LONG):
-            print('- skipping range: covers an account in {}'.format(TAKES_TOO_LONG))
-            continue
-        res = __query_account_id_range(start, end).strip()
-        if res:
-            print(res)
-        print('- ran from {} to {}'.format(start, end))
+    asyncio.run(__run_jobs(), debug=True)
 
 
 main()
--- bin/dev/bin/download_es_data.sh
+++ bin/dev/bin/download_es_data.sh
@@ -1,8 +1,8 @@
-# requires `pip install es2csv`
+# requires `pip install es2csv`, which requires python 2.7
 
 es2csv \
-    --output-file new_products_data_dump.csv \
-    --url https://vpc-aws1-perftest-one-1804-es-hywlozme3rdpytwa5zo2eidmdy.us-east-1.es.amazonaws.com \
+    --output-file products_data_dump_2019-08-08.csv \
+    --url https://vpc-aws4-perftest-four-1305-es-5oy2l4wk4yvffjy23l7kw5rdle.us-east-1.es.amazonaws.com \
     --index-prefixes mc-product \
     --max 0 \
     --query '*'
