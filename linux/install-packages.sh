d1f984a1fe9d7be5620371e19fb704920d13ef52
Topher Brown <t.brown@wordstream.com>
2019-12-11 07:37:17 +0000

    install 'pet'
    Former-commit-id: a048f084f08db34373bd1fca375a340837ab4f7a

--- linux/install-packages.sh
+++ linux/install-packages.sh
@@ -7,3 +7,7 @@ sudo apt install -y \
 # install python packages
 sudo apt install -y python3-dev python3-pip python3-setuptools
 sudo pip3 install thefuck
+
+# pet snippet manager
+wget https://github.com/knqyf263/pet/releases/download/v0.3.6/pet_0.3.6_linux_amd64.deb
+sudo dpkg -i pet_0.3.6_linux_amd64.deb
