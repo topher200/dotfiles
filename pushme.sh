99364586a5e296b9282065bf2f5cc0188f4d7536
Topher Brown <topherb@google.com>
2020-06-09 19:27:41 -0700

    update warning on 'pushme'
    Former-commit-id: 0ff1786c24562933fd3bb7d05340edf77b2ac4fb

--- pushme.sh
+++ pushme.sh
@@ -3,15 +3,16 @@
 # quit the script immediately on error
 set -e
 
-echo 'WARNING! this will push HEAD to github. <enter> to proceed'
+echo 'WARNING! this will push HEAD to github (secrets!) AND THEN force push to google. <enter> to proceed'
 git log --oneline --decorate=full | head
 read -n 1
 
-echo 'push current branch to github repo'
+echo 'push HEAD to github repo'
 git push github HEAD:master
 
 echo 'rebase google/master branch on top of HEAD'
 git checkout master
 git rebase github/master
-echo 'push google to google repo'
-git push origin HEAD:master
+
+echo 'FORCE PUSH master to google repo'
+git push origin HEAD:master --force
