9da36b93790c371d8c7ad6bd22776d464441b1f2
Topher Brown <topher@memfault.com>
2021-06-28 03:27:44 +0000

    Move clone command into script

---
+++ scripts/gitpod-startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# copy gitpod's gitconfig so that we can use their auto-generated credentials
+sudo cp /home/gitpod/.gitconfig /etc/gitconfig
+
+# log into 'topher'
+sudo su - topher
+
+# replace the neutered 'dotfiles' clone with a real one
+rm -r dotfiles
+git clone https://github.com/topher200/dotfiles
+cd dotfiles
+
+# re-run stow to fix symlinks
+make stow
+
+# startup is a session in tmux
+tmux new-sess -A -s work
