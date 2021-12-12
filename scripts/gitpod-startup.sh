e853a5831b4d145cc43f7436c1137dfed80280ac
Topher Brown <topher@memfault.com>
2021-12-12 04:49:07 +0000

    Stop using 'sudo su -- topher' in gitpod startup

--- scripts/gitpod-startup.sh
+++ scripts/gitpod-startup.sh
@@ -4,16 +4,13 @@ set -ex
 # copy gitpod's gitconfig so that we can use their auto-generated credentials
 sudo cp /home/gitpod/.gitconfig /etc/gitconfig
 
-# log into 'topher'
-sudo su - topher
-
-# replace the neutered 'dotfiles' clone with a real one
-rm -r dotfiles
-git clone https://github.com/topher200/dotfiles
-cd dotfiles
+# replace the neutered 'dotfiles' clone with a real one.
+sudo rm -r /home/topher/dotfiles
+# sudo-ing into the 'topher' user wasn't working correctly so we do single-use commands.
+sudo su -c 'git clone https://github.com/topher200/dotfiles /home/topher/dotfiles' -- topher
 
 # re-run stow to fix symlinks
-make stow
+sudo su -c 'make -C /home/topher/dotfiles stow' -- topher
 
-# startup is a session in tmux
-tmux new-sess -A -s work
+# startup a new tmux session as 'topher'
+sudo su -c 'tmux new-sess -A -s work' -- topher
