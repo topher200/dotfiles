709252787be4d7d3343e8973328e5e2c95a88c90
Topher Brown <topher@memfault.com>
2022-01-30 22:40:13 -0500

    ~/dotfiles -> ~/dev/dotfiles

--- scripts/gitpod-startup.sh
+++ scripts/gitpod-startup.sh
@@ -5,12 +5,12 @@ set -ex
 sudo cp /home/gitpod/.gitconfig /etc/gitconfig
 
 # replace the neutered 'dotfiles' clone with a real one.
-sudo rm -r /home/topher/dotfiles
+sudo rm -r /home/topher/dev/dotfiles
 # sudo-ing into the 'topher' user wasn't working correctly so we do single-use commands.
-sudo su -c 'git clone https://github.com/topher200/dotfiles /home/topher/dotfiles' -- topher
+sudo su -c 'git clone https://github.com/topher200/dotfiles /home/topher/dev/dotfiles' -- topher
 
 # re-run stow to fix symlinks
-sudo su -c 'make -C /home/topher/dotfiles stow' -- topher
+sudo su -c 'make -C /home/topher/dev/dotfiles stow' -- topher
 
 # startup a new tmux session as 'topher'
 sudo su - topher
