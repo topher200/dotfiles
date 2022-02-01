b8361a5c851ce6c51311c2688201b014fe7b2aec
Topher Brown <topher@memfault.com>
2022-02-01 00:27:40 -0500

    Install build-essentials via apt

--- install-packages.sh
+++ install-packages.sh
@@ -7,6 +7,7 @@ set -Eeuox pipefail
 sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
     autojump \
+    build-essential \
     colorized-logs \
     curl \
     docker \
