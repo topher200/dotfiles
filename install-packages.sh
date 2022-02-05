d0abcfb887013aaf68625f2b55eba95383d6bea4
Topher Brown <topher@memfault.com>
2022-02-05 17:50:36 -0500

    --keep-zshrc

--- install-packages.sh
+++ install-packages.sh
@@ -89,7 +89,7 @@ fi
 
 # install oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 fi
 
 # install vim plugins
