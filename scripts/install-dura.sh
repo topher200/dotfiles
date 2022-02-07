74f9e4d6d42e2e1f5990f09c2df6a83b6ecb14b6
Topher Brown <topher@memfault.com>
2022-02-07 11:59:07 -0500

    pre-commit fixes

--- scripts/install-dura.sh
+++
@@ -1,6 +0,0 @@
-brew install rustup && brew install rust
-git clone git@github.com:tkellogg/dura.git
-cd dura
-# Note: If you receive a failure fetching the cargo dependencies try using the local git client for cargo fetches.
-# cargo install --path
-CARGO_NET_GIT_FETCH_WITH_CLI=true cargo install --path .
