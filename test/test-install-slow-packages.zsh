edff5bd4c373f0eec8c5cf652e5695291a68fc4b
Topher Brown <206988+topher200@users.noreply.github.com>
2022-06-05 16:17:15 -0600

    Install-lazydocker (#82)

---
+++ test/test-install-slow-packages.zsh
@@ -0,0 +1,30 @@
+#!/usr/bin/zsh
+# shellcheck shell=bash
+
+set -Eeuo pipefail
+
+exists() {
+	command -v "$1" >/dev/null 2>&1
+}
+
+fail_if_not_exists() {
+	if ! exists "$1"; then
+		echo "$1" not found
+		exit 1
+	fi
+}
+
+fail_if_not_exists brew
+fail_if_not_exists circleci
+fail_if_not_exists gcc
+fail_if_not_exists lazydocker
+fail_if_not_exists lazygit
+fail_if_not_exists jless
+fail_if_not_exists pre-commit
+fail_if_not_exists prettier
+fail_if_not_exists shfmt
+fail_if_not_exists thefuck
+fail_if_not_exists tmuxp
+fail_if_not_exists tldr
+fail_if_not_exists graphite
+fail_if_not_exists gt
