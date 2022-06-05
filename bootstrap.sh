9e344140e1d34150d93f8b35769950ab404f8d22
Topher Brown <topher@memfault.com>
2022-06-05 00:20:05 -0400

    remove garbage gitpod files from stow paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,17 @@
 set -ex
 
 make install-fast-packages
+
+# gitpod provides a garbage ~/.zshrc file
+echo removing ~/.zshrc
+cat ~/.zshrc
+rm ~/.zshrc
+
+# gitpod stores git credentials in .gitconfig. we need those! save them and add
+# them back in later
+mv ~/.gitconfig ~/.gitconfig.gitpod
+
 make
+
+echo concatenating  ~/.gitconfig together
+cat ~/.gitconfig.gitpod >> ~/.gitconfig
