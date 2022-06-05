a860b4f748c4ff5931d10d00f018f99a85cbfbd5
Topher Brown <206988+topher200@users.noreply.github.com>
2022-06-05 16:52:57 -0600

    Add editorconfig from work (#86)

--- scripts/install-fast-packages.sh
+++ scripts/install-fast-packages.sh
@@ -7,65 +7,65 @@ sudo add-apt-repository ppa:git-core/ppa -y
 # timezone copy and DEBIAN_FRONTEND are to stop 'tzdata' from prompting for timezone during install
 sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
-    autojump \
-    build-essential \
-    colorized-logs \
-    curl \
-    docker \
-    fd-find \
-    file \
-    flameshot \
-    fzf \
-    htop \
-    httpie \
-    git \
-    moreutils \
-    neovim \
-    jq \
-    pspg \
-    shellcheck \
-    silversearcher-ag \
-    stow \
-    tig \
-    tmux \
-    tree \
-    unp \
-    unzip \
-    vim \
-    wget \
-    xsel \
-    zsh
+	autojump \
+	build-essential \
+	colorized-logs \
+	curl \
+	docker \
+	fd-find \
+	file \
+	flameshot \
+	fzf \
+	htop \
+	httpie \
+	git \
+	moreutils \
+	neovim \
+	jq \
+	pspg \
+	shellcheck \
+	silversearcher-ag \
+	stow \
+	tig \
+	tmux \
+	tree \
+	unp \
+	unzip \
+	vim \
+	wget \
+	xsel \
+	zsh
 
 # workaround for https://github.com/sharkdp/bat/issues/938, required for ubuntu 20.04 (but not later!)
 sudo apt-get install -y -o Dpkg::Options::="--force-overwrite" bat ripgrep
 
 # TODO: use this for more checks
 command_exists() {
-    command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
 # pet snippet manager
 if [ ! -f /tmp/pet.deb ]; then
-    wget https://github.com/knqyf263/pet/releases/download/v0.3.6/pet_0.3.6_linux_amd64.deb -O /tmp/pet.deb
-    sudo dpkg -i /tmp/pet.deb
+	wget https://github.com/knqyf263/pet/releases/download/v0.3.6/pet_0.3.6_linux_amd64.deb -O /tmp/pet.deb
+	sudo dpkg -i /tmp/pet.deb
 fi
 
 # install viddy, from https://github.com/sachaos/viddy
 if [ ! -f /usr/local/bin/viddy ]; then
-    wget -O /tmp/viddy.tar.gz https://github.com/sachaos/viddy/releases/download/v0.3.0/viddy_0.3.0_Linux_x86_64.tar.gz
-    pushd /tmp || exit
-    tar xvf /tmp/viddy.tar.gz
-    sudo mv /tmp/viddy /usr/local/bin
-    popd || exit
+	wget -O /tmp/viddy.tar.gz https://github.com/sachaos/viddy/releases/download/v0.3.0/viddy_0.3.0_Linux_x86_64.tar.gz
+	pushd /tmp || exit
+	tar xvf /tmp/viddy.tar.gz
+	sudo mv /tmp/viddy /usr/local/bin
+	popd || exit
 fi
 
 # install exa, (not available on Ubuntu 20.04)
 if [ ! -f /usr/local/bin/exa ]; then
-    wget -O /tmp/exa.zip https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip
-    pushd /tmp || exit
-    unp exa.zip
-    sudo mv bin/exa /usr/local/bin
-    popd || exit
+	wget -O /tmp/exa.zip https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip
+	pushd /tmp || exit
+	unp exa.zip
+	sudo mv bin/exa /usr/local/bin
+	popd || exit
 fi
 
 # set shell to zsh
