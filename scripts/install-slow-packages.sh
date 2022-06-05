460fb55b08c1a9a0f955f8b959ea3bb1a2226c6d
Topher Brown <topher@memfault.com>
2022-06-05 18:39:02 -0400

    Prettier updates

--- scripts/install-slow-packages.sh
+++ scripts/install-slow-packages.sh
@@ -6,24 +6,24 @@
 set -Eeuox pipefail
 
 command_exists() {
-    command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
 # install Linuxbrew
 if ! command_exists brew; then
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 brew install \
-    circleci \
-    gcc \
-    jesseduffield/lazydocker/lazydocker \
-    jesseduffield/lazygit/lazygit \
-    jless \
-    pre-commit \
-    prettier \
-    shfmt \
-    thefuck \
-    tmuxp \
-    tldr \
-    screenplaydev/tap/graphite
+	circleci \
+	gcc \
+	jesseduffield/lazydocker/lazydocker \
+	jesseduffield/lazygit/lazygit \
+	jless \
+	pre-commit \
+	prettier \
+	shfmt \
+	thefuck \
+	tmuxp \
+	tldr \
+	screenplaydev/tap/graphite
--- test/run_shellcheck.sh
+++ test/run_shellcheck.sh
@@ -9,30 +9,30 @@ ERRORS=()
 # find all executables and run `shellcheck` on them
 # define excludes here:
 for f in $(find . -type f \
-    -not -iwholename '*.git*' \
-    -not -iwholename './files/.oh-my-zsh/*' \
-    -not -iwholename './files/.tmux/plugins/*' \
-    -not -iwholename './files/bin/dropbox_uploader.sh' \
-    -not -iwholename './files/bin/ftwind' \
-    -not -iwholename './files/bin/vimv' \
-    -not -iwholename './files/zsh-custom/*' \
-    -not -iwholename './node_modules/*' \
-    | sort -u); do
+	-not -iwholename '*.git*' \
+	-not -iwholename './files/.oh-my-zsh/*' \
+	-not -iwholename './files/.tmux/plugins/*' \
+	-not -iwholename './files/bin/dropbox_uploader.sh' \
+	-not -iwholename './files/bin/ftwind' \
+	-not -iwholename './files/bin/vimv' \
+	-not -iwholename './files/zsh-custom/*' \
+	-not -iwholename './node_modules/*' |
+	sort -u); do
 
-    if file "$f" | grep --quiet shell; then
-        {
-            shellcheck "$f" && echo "[OK]: sucessfully linted $f"
-        } || {
-            # add to errors
-            ERRORS+=("$f")
-        }
-    fi
+	if file "$f" | grep --quiet shell; then
+		{
+			shellcheck "$f" && echo "[OK]: sucessfully linted $f"
+		} || {
+			# add to errors
+			ERRORS+=("$f")
+		}
+	fi
 done
 
 if [ ${#ERRORS[@]} -eq 0 ]; then
-    echo "No errors, hooray"
+	echo "No errors, hooray"
 else
-    echo "These files failed shellcheck: "
-    printf '  %s\n' "${ERRORS[@]}"
-    exit 1
+	echo "These files failed shellcheck: "
+	printf '  %s\n' "${ERRORS[@]}"
+	exit 1
 fi
