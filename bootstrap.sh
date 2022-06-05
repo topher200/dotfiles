7f0e3bdeabedd8e232c0582905c22b9969fd5bfe
Topher Brown <206988+topher200@users.noreply.github.com>
2022-06-05 16:52:30 -0600

    Add timing to bootstrap script (#87)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,9 @@ set -Eeuox pipefail
 
 cd /home/gitpod/.dotfiles
 
+echo Installing fast packages at "$(date)"
 make install-fast-packages
+echo Finished installing fast packages at "$(date)"
 
 # gitpod provides garbage ~/.zshrc and .gitconfig files
 echo removing ~/.zshrc
@@ -13,4 +15,6 @@ echo removing ~/.gitconfig
 cat ~/.gitconfig
 rm ~/.gitconfig
 
+echo Running stow at "$(date)"
 make
+echo Finished stow at "$(date)"
