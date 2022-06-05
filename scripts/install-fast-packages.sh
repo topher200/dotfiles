8e0e6ac534a33f3a4538f87c225e361adb2c17bf
Topher Brown <topher@memfault.com>
2022-06-05 00:21:32 -0400

    remove "apt-get update" from fast script

--- scripts/install-fast-packages.sh
+++ scripts/install-fast-packages.sh
@@ -6,7 +6,7 @@ set -Eeuox pipefail
 sudo add-apt-repository ppa:git-core/ppa -y
 # timezone copy and DEBIAN_FRONTEND are to stop 'tzdata' from prompting for timezone during install
 sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
     autojump \
     build-essential \
     colorized-logs \
