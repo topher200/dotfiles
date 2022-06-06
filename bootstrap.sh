0e09aec5000dac0d20fddda0a8a6859be8381748
Topher Brown <topher@memfault.com>
2022-06-06 13:31:06 -0400

    Keep existing .gitconfig for gitpod

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,14 +10,18 @@ echo Installing fast packages at "$(date)"
 make install-fast-packages
 echo Finished installing fast packages at "$(date)"
 
-# gitpod provides garbage ~/.zshrc and .gitconfig files
+# gitpod provides a garbage ~/.zshrc file
 echo removing ~/.zshrc
 cat ~/.zshrc
 rm ~/.zshrc
-echo removing ~/.gitconfig
-cat ~/.gitconfig
-rm ~/.gitconfig
+
+# gitpod stores git credentials in .gitconfig. we need those! save them and add
+# them back in later
+mv ~/.gitconfig ~/.gitconfig.gitpod
 
 echo Running stow at "$(date)"
 make
 echo Finished stow at "$(date)"
+
+echo concatenating ~/.gitconfig together
+cat ~/.gitconfig.gitpod >> ~/.gitconfig
