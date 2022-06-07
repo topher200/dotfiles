ce81545f6bd8f76bd6a4a3a6e0ab38051d09abf3
Topher Brown <206988+topher200@users.noreply.github.com>
2022-06-06 19:58:41 -0600

    Configure graphite with sgp (#105)

--- start-tmux-and-install-slow-packages.sh
+++ start-tmux-and-install-slow-packages.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# configure graphite
+echo '{"trunk": "master"}' >.git/.graphite_repo_config
+
+# start tmux session, with background windows doing setup
 tmux new-session -d -s work
 tmux new-window -n install-slow 'cd ~/.dotfiles && make install-slow-packages'
 tmux select-window -t 1
+
+# connect to tmux session
 tmux attach-session -d -t work
