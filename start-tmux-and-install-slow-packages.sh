0473e4cb8d245bfc8b95578228af98d28414a6d0
Topher Brown <206988+topher200@users.noreply.github.com>
2022-06-06 21:10:33 -0600

    Switch to dotfiles repo only if needed (#110)
    We're running into an issue on our dotfiles repo where we're installing
    older versions of the scripts. Use the latest version available!

--- start-tmux-and-install-slow-packages.sh
+++ start-tmux-and-install-slow-packages.sh
@@ -6,7 +6,12 @@ echo "{\"authToken\": \"$GRAPHITE_AUTH_TOKEN\", \"branchPrefix\": \"topher200/\"
 
 # start tmux session, with background windows doing setup
 tmux new-session -d -s work
-tmux new-window -n install-slow 'cd ~/.dotfiles && make install-slow-packages && pre-commit install --install-hooks'
+if [[ -d /workspace/dotfiles ]]; then
+	tmux new-window -n install-slow 'cd /workspace/dotfiles && make install-slow-packages && pre-commit install --install-hooks'
+else
+	# if we're not already in a dotfiles repo, find one
+	tmux new-window -n install-slow 'cd ~/.dotfiles && make install-slow-packages && pre-commit install --install-hooks'
+fi
 tmux select-window -t 1
 
 # connect to tmux session
