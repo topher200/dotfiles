d6c028e710a433547a69f86bd400d2d13335eb51
Topher Brown <topher@memfault.com>
2022-06-25 18:13:45 -0400

    do not move pure-prompt

--- update_external_dependencies.sh
+++ update_external_dependencies.sh
@@ -26,13 +26,6 @@ git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting files/z
 rm -rf files/zsh-custom/zsh-syntax-highlighting/.git
 echo 'done upgrading zsh-syntax-highlighting'
 
-echo 'upgrading pure-prompt'
-# https://github.com/sindresorhus/pure
-rm -r files/zsh-custom/pure
-git clone --depth 1 https://github.com/sindresorhus/pure files/zsh-custom/pure
-rm -rf files/zsh-custom/pure/.git
-echo 'done upgrading pure-prompt'
-
 echo 'upgrading tmux plugin manager (tpm)'
 rm -r files/.tmux/plugins/tpm
 git clone --depth 1 https://github.com/tmux-plugins/tpm files/.tmux/plugins/tpm
