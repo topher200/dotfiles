35fb03d668134f796f0a243f8291679a8effdc7f
Topher Brown <206988+topher200@users.noreply.github.com>
2022-12-22 14:44:21 -0500

    optional script to install node (#179)
    * optional script to install node
    
    * turn off shellcheck following node files

---
+++ scripts/optional-install-node.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -Eeuox pipefail
+
+# We don't need to install node/npm, but if we do want to we should install it
+# with nvm (which is like npm, but keeps global installs local to the current
+# user).
+
+# install nvm
+# from https://github.com/nvm-sh/nvm#installing-and-updating
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+# set up nvm in the current shell.
+# (we could have also just spawned a new shell)
+export NVM_DIR="$HOME/.nvm"
+# shellcheck disable=SC1090
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# shellcheck disable=SC1090
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# install node
+nvm install node
