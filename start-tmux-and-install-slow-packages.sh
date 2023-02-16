2bb069dd7d00638f05513850ed67465b6a00a296
Topher Brown <206988+topher200@users.noreply.github.com>
2023-02-15 21:18:02 -0500

    Log into docker hub (#188)
    * stop aliasing "cat"
    
    * tmux copy-mode-vi searches in the expected direction
    
    * reorder tmux "work" panes
    
    * Update git alias for "g dom"
    
    * Log into docker hub

--- start-tmux-and-install-slow-packages.sh
+++ start-tmux-and-install-slow-packages.sh
@@ -7,6 +7,14 @@ set -Eeuox pipefail
 echo '{"trunk": "master"}' >.git/.graphite_repo_config
 echo "{\"authToken\": \"$GRAPHITE_AUTH_TOKEN\", \"branchPrefix\": \"topher/\", \"submitIncludeCommitMessages\": true}" >~/.graphite_user_config
 
+# configure docker hub
+if [[ -z "${DOCKER_HUB_USERNAME:-}" ]] || [[ -z "${DOCKER_HUB_PAT:-}" ]]; then
+	echo 'DOCKER_HUB_USERNAME and DOCKER_HUB_PAT not set, skipping docker login'
+else
+	echo 'DOCKER_HUB_USERNAME and DOCKER_HUB_PAT set, running docker login'
+	echo "$DOCKER_HUB_PAT" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+fi
+
 # start tmux session, with background windows doing setup
 tmux new-session -d -s work
 if [[ -d /workspace/dotfiles ]]; then
