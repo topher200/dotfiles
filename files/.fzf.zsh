02436a1ad2b170aaa144c4ae136cc2bedd1e786b
Topher Brown <206988+topher200@users.noreply.github.com>
2023-06-24 15:12:21 -0400

    add fzf.zsh file (#231)

---
+++ files/.fzf.zsh
@@ -0,0 +1,14 @@
+# shellcheck shell=bash
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/linuxbrew/.linuxbrew/opt/fzf/bin* ]]; then
+	PATH="${PATH:+${PATH}:}/home/linuxbrew/.linuxbrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/linuxbrew/.linuxbrew/opt/fzf/shell/completion.zsh" 2>/dev/null
+
+# Key bindings
+# ------------
+source "/home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.zsh"
