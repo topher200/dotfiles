d57a0b68a18bcf39a1c95cdda69a24a82da59fd8
Topher Brown <206988+topher200@users.noreply.github.com>
2024-02-11 10:52:39 -0500

    Codespaces dotfiles setup

--- bootstrap-codespace.sh
+++ bootstrap-codespace.sh
@@ -4,3 +4,11 @@ set -Eeuox pipefail
 echo Installing fast packages at "$(date)"
 make install-fast-packages || true
 echo Finished installing fast packages at "$(date)"
+
+# use our .zshrc file, not codespace's
+echo removing ~/.zshrc
+mv ~/.zshrc ~/.zshrc.codespace-orig.bak
+
+echo Running stow at "$(date)"
+make
+echo Finished stow at "$(date)"
