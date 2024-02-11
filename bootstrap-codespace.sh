1d577d43b887eff53848a879f3d5157040b925bf
Topher Brown <topher@memfault.com>
2024-02-11 13:28:11 -0500

    Move .oh-my-zsh in codespace

--- bootstrap-codespace.sh
+++ bootstrap-codespace.sh
@@ -5,13 +5,13 @@ echo Installing fast packages at "$(date)"
 make install-fast-packages || true
 echo Finished installing fast packages at "$(date)"
 
-# use our .zshrc file, not codespace's
+# use our files, not codespace's
 echo removing ~/.zshrc
 mv ~/.zshrc ~/.zshrc.bak || true
-
-# use our .gitconfig file, not codespace's
 echo removing ~/.gitconfig
 mv ~/.gitconfig ~/.gitconfig.bak || true
+echo removing ~/.oh-my-zsh
+mv ~/.oh-my-zsh ~/.oh-my-zsh.bak || true
 
 echo Running stow at "$(date)"
 make
